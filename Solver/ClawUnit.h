303f873a477db9429267915d42a73b028c51cef3
NikolayTomshin <90717511+NikolayTomshin@users.noreply.github.com>
2023-02-01 11:43:18 +0300

    Updoot

--- Solver/ClawUnit.h
+++ Solver/ClawUnit.h
@@ -4,38 +4,46 @@
 #include <Servo.h>
 #include "Mathclasses.h"
 class ClawUnit : IUpdatable {
-  uint8_t mD, mP, minP, A, B, state;
+  uint8_t mD, mP, minP, A, B, rstate, gstate, adP;
   Servo S;
 public:
   ClawUnit() {}
-  ClawUnit(uint8_t md, uint8_t mp, uint8_t minp, uint8_t sc, uint8_t a, uint8_t b) {
+  ClawUnit(uint8_t md, uint8_t mp, uint8_t minp, uint8_t adp, uint8_t sc, uint8_t a, uint8_t b) {
     mD = md;
     mP = mp;
     A = a;
     B = b;
     minP = minp;
+    adP = adp;
     S.attach(sc);
     pinMode(mD, OUTPUT);
     pinMode(mP, OUTPUT);
   }
   void grab() {
     S.write(180);
+    gstate = 2;
   }
   void release() {
     S.write(0);
+    gstate = 0;
   }
   void loosen() {
     S.write(175);
+    gstate = 1;
+  }
+  void toggle() {
+    if (gstate > 0) release();
+    else loosen();
   }
   void stop() {
     digitalWrite(mP, LOW);
   }
   void go(bool dir) {
-    digitalWrite(mD,dir);
-    analogWrite(mP, minP);
+    digitalWrite(mD, dir);
+    analogWrite(mP, minP + adP * gstate);
   }
-  void run(bool dir){
-    digitalWrite(mD,dir);
+  void run(bool dir) {
+    digitalWrite(mD, dir);
     analogWrite(mP, 255);
   }
   bool a() {
@@ -45,9 +53,9 @@ public:
     return (digitalRead(B));  //верхний канал   '_'.
   }
   void Update() {
-    state = a()+2*b();
+    rstate = a() + 2 * b();
   }
-  uint8_t getState(){
-    return(state);
+  uint8_t getState() {
+    return (rstate);
   }
 };
--- Solver/Solver.ino
+++ Solver/Solver.ino
@@ -1,52 +1,71 @@
-#include <Nextion.h>  // подключаем библиотеку
+#include <Nextion.h>
+
 #include "ClawUnit.h"
 #include "TimeManager.h"
-uint8_t s;
-ClawUnit n1(7, 6, 115, 9, A0, A1);
-ClawUnit n2(4, 5, 130, 10, A2, A3);
-bool a = 0;
-
 
 NexButton b0 = NexButton(0, 1, "b0");
-NexButton b1 = NexButton(0, 2, "b1");         //страница дисплея, id и имя объекта
+NexButton b1 = NexButton(0, 2, "b1");  //страница дисплея, id и имя объекта
 NexButton b2 = NexButton(0, 3, "b2");
-NexButton b3 = NexButton(0, 4, "b3");         //страница дисплея, id и имя объекта
+NexButton b3 = NexButton(0, 4, "b3");  //страница дисплея, id и имя объекта
 NexButton b4 = NexButton(0, 5, "b4");
-NexButton b5 = NexButton(0, 6, "b5");         //страница дисплея, id и имя объекта
-NexTouch *nex_listen_list[] = { &b0, &b1, &b2, &b3, &b4, &b5 };  //массив ссылок на кнопки
+NexButton b5 = NexButton(0, 6, "b5");
+
+
+ClawUnit n1;
+ClawUnit n2;
+NexTouch *nex_listen_list[] = { &b0, &b1, &b2, &b3, &b4, &b5 };  //массив ссылок на кнопки 
 
 void b0PushCallback(void *ptr) {
   n1.toggle();
 }
-void b1PushCallback(void *ptr) {
-  n1.go(true);
-}
 void b2PushCallback(void *ptr) {
   n2.toggle();
 }
-void b3PushCallback(void *ptr) {
-  n2.go(true);
+
+void b1PushCallback(void *ptr) {
+  n1.go(true);
 }
 void b4PushCallback(void *ptr) {
   n1.go(false);
 }
-void b5PushCallback(void *ptr) {
-  n2.go(false);
+void b1PopCallback(void *ptr) {
+  n1.stop();
 }
+void b4PopCallback(void *ptr) {
+  n1.stop();
+}
+
+void b3PushCallback(void *ptr) {
+  n2.run(true);
+}
+void b5PushCallback(void *ptr) {
+  n2.run(false);
+}
+void b3PopCallback(void *ptr) {
+  n2.stop();
+}
+void b5PopCallback(void *ptr) {
+  n2.stop();
+}
+
 void setup() {
-  
-  Serial.begin(9600);
+  Serial1.begin(9600);
+  delay(100);
+  n1=ClawUnit(4, 5, 130, 60, 10, A0, A1);
+  n2=ClawUnit(7, 6, 130, 60,  9, A2, A3);
+  b0.attachPush(b0PushCallback);
+  b1.attachPush(b1PushCallback);
+  b1.attachPop(b1PopCallback);
+  b2.attachPush(b2PushCallback);
+  b3.attachPush(b3PushCallback);
+  b3.attachPop(b3PopCallback);
+  b4.attachPush(b4PushCallback);
+  b4.attachPop(b4PopCallback);
+  b5.attachPush(b5PushCallback);
+  b5.attachPop(b5PopCallback);
   n2.release();
 }
 void loop() {
-  n2.go(1);
-  delay(5000);
-  n2.stop();
-  delay(10000);
-  // n2.Update();
-  // uint8_t ns = n2.getState();
-  // if (ns != s) {
-  //   Serial.println(ns);
-  //   s = ns;
-  // }
+  nexLoop(nex_listen_list);
+  // delay(15);
 }
